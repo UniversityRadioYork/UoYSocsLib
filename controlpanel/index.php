e729441fec062f5927393e4b865705fc0a50e254
Gareth Andrew Lloyd <gareth@ignition-web.co.uk>
2011-07-08 11:50:13 +0100

    added jquery ui

--- controlpanel/index.php
+++ controlpanel/index.php
@@ -3,11 +3,11 @@
 //Authentication code here
 
 ?><!doctype html>
-<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
-<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
-<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
-<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<!--[if lt IE 7 ]> <html lang="en" class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class=""> <!--<![endif]-->
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -20,10 +20,13 @@
 	
 	<link rel="shortcut icon" href="/favicon.ico">
 	<link rel="apple-touch-icon" href="/apple-touch-icon.png">
-	<link rel="stylesheet" href="css/style.css?v=2">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/jquery-ui.min.js"></script>
-	<script src="js/libs/modernizr-1.7.min.js"></script></head>
+	<!--<link type="text/css" rel="stylesheet" href="css/style.css?v=2" />-->
+	<link type="text/css" rel="stylesheet" href="css/ui-lightness/jquery-ui-1.8.14.custom.css" />
+
+	<script type="text/javascript" src="js/libs/jquery-1.5.1.min.js"></script>
+	<script type="text/javascript" src="js/libs/jquery-ui-1.8.14.custom.min.js"></script>
+	<script type="text/javascript" src="js/libs/modernizr-1.7.min.js"></script>
+</head>
 <body>
 	<div id="container">
 		<header>
@@ -31,7 +34,22 @@
 		</header>
 
 		<div id="main" role="main">
-
+			<div id="tabs">
+				<ul>
+					<li><a href="#tabs-1">Term Dates</a></li>
+					<li><a href="#tabs-2">Update Sources</a></li>
+					<li><a href="#tabs-3">Help</a></li>
+				</ul>
+				<div id="tabs-1">
+					<p>Tab 1 content</p>
+				</div>
+				<div id="tabs-2">
+					<p>Tab 2 content</p>
+				</div>
+				<div id="tabs-3">
+					<p>Tab 3 content</p>
+				</div>
+			</div>
 		</div>
 
 		<footer>
@@ -39,8 +57,8 @@
 		</footer>
 	</div>
 
-  <script src="js/plugins.js"></script>
-  <script src="js/script.js"></script>
+	<script type="text/javascript" src="js/plugins.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
 	<!--[if lt IE 7 ]>
 	<script src="js/libs/dd_belatedpng.js"></script>
 	<script> DD_belatedPNG.fix('img, .png_bg');</script>
